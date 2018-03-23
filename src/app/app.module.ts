0eef8c6cd5418a4afda36c635263208fc6c1d5be
Ionut Robert <ionut.iacob17@gmail.com>
2018-03-23 15:51:10 +0200

    Change ng-socket.io to sockjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { OverlayModule } from '@angular/cdk/overlay';
 
-import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './shared/core';
 import { SidemenuModule, SidemenuService } from './shared/sidemenu';
 import { SocketService } from './shared/services/socket.service';
-
-const config: SocketIoConfig = { url: 'http://localhost:8080/reservations', options: {} };
 
 @NgModule({
   declarations: [
@@ -20,7 +16,6 @@ const config: SocketIoConfig = { url: 'http://localhost:8080/reservations', opti
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SocketIoModule.forRoot(config),
     OverlayModule,
     CoreModule,
     SidemenuModule
--- src/app/shared/services/socket.service.ts
+++ src/app/shared/services/socket.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from "@angular/core";
 import { Socket } from 'ng-socket-io';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Injectable()
 export class SocketService {
 
-	constructor(private socket: Socket) {
-		this.socket
-            .fromEvent("message")
-            .subscribe((res)=>{
-				console.log("Connected");
-				console.log(res);
-			});
+	// private serverUrl = 'http://localhost:8080/reservations'
+	private serverUrl = 'http://localhost:8080/reservations';
+	private title = 'WebSockets chat';
+	private stompClient;
+
+	constructor() {
+		let ws = new SockJS(this.serverUrl);
+		this.stompClient = Stomp.over(ws);
+		let self = this;
+		console.log('trying to connect');
+		this.stompClient.connect({
+			transports: ['websocket']
+		  }, function (frame) {
+			console.log('connected');
+			console.log(frame);
+			// self.stompClient.subscribe("/reservations", (message) => {
+
+			// });
+		});
 	}
 
 }
